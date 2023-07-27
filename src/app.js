9ff33837807cf531069a7d1f721931fd5471ef1f
Julio Flores <julio.flores.1089@gmail.com>
2023-07-27 15:14:24 -0400

    delete unused library

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
 const { config } = require('dotenv');
 const { router } = require('./routes');
 const { errorHandler } = require('./errors/index');
@@ -13,11 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cookieParser());
-
-// app.get('/api/health', (_, res) => {
-//   res.send('server PlayStudios Ok');
-// });
 
 app.use('/api', router);
 
